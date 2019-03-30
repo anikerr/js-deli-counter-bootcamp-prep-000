48e4e7d3ff91b941339a89bdb2e596f5e1b835e4
Alana Kerr <ani.kerr@gmail.com>
2019-03-30 21:17:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-function takeANumber(currentLine, newName) {
-  return `Welcome, ${newName}. You are number ${}`
+function takeANumber(katzDeliLine, newName) {
+  return `Welcome, ${newName}. You are number ${katzDeliLine.indexOf(newName) + 1} in line.`;
+}
+
+function nowServing(katzDeliLine) {
+  return 
 }
